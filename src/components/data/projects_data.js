e5c5b8c7b335d454e9d837e1536939cb5d9670f1
= <=>
2022-08-28 19:21:30 -0230

    added  cloudnotebook

--- src/components/data/projects_data.js
+++ src/components/data/projects_data.js
@@ -1,180 +1,177 @@
-import Spotify from '../../assets/projects_images/Spotify.png'
-import Musicxy from '../../assets/projects_images/Musicxy.jpg'
-import Todo from '../../assets/projects_images/Todo.png'
-import Encryption from '../../assets/projects_images/Encryption.jpg'
-import Notifier from '../../assets/projects_images/Notifier.png'
-import Gym from '../../assets/projects_images/Gym.png'
-import Loginpage from '../../assets/projects_images/Loginpage.png'
-import Covidnotify from '../../assets/projects_images/Covidnotify.jpg'
-import Covidmap from '../../assets/projects_images/Covidmap.png'
-import Jarvis from '../../assets/projects_images/Jarvis.jpg'
-import Dice from '../../assets/projects_images/Dice.png'
-import Imagesearch from '../../assets/projects_images/Imagesearch.png'
-import Chat from '../../assets/projects_images/Chat.png'
-import Form from '../../assets/projects_images/Form.png'
-import KingDomino from '../../assets/projects_images/KingDomino.png'
-import Textutils from '../../assets/projects_images/Textutils.png'
-import NewsMonkey from '../../assets/projects_images/NewsMonkey.png'
+import Spotify from "../../assets/projects_images/Spotify.png";
+import Musicxy from "../../assets/projects_images/Musicxy.jpg";
+import Todo from "../../assets/projects_images/Todo.png";
+import Encryption from "../../assets/projects_images/Encryption.jpg";
+import Notifier from "../../assets/projects_images/Notifier.png";
+import Gym from "../../assets/projects_images/Gym.png";
+import Loginpage from "../../assets/projects_images/Loginpage.png";
+import Covidnotify from "../../assets/projects_images/Covidnotify.jpg";
+import Covidmap from "../../assets/projects_images/Covidmap.png";
+import Jarvis from "../../assets/projects_images/Jarvis.jpg";
+import Dice from "../../assets/projects_images/Dice.png";
+import Imagesearch from "../../assets/projects_images/Imagesearch.png";
+import Chat from "../../assets/projects_images/Chat.png";
+import Form from "../../assets/projects_images/Form.png";
+import KingDomino from "../../assets/projects_images/KingDomino.png";
+import Textutils from "../../assets/projects_images/Textutils.png";
+import NewsMonkey from "../../assets/projects_images/NewsMonkey.png";
+import CloudNoteBook from "../../assets/projects_images/CloudNoteBook.png";
 
 const data_projects = [
+  {
+    name: "CloudNoteBook",
+    image: CloudNoteBook,
+    deployed_url: "https://cloudnotebooks.netlify.app/",
+    github_url: "https://github.com/AbhishekGujjar17/CloudNoteBook",
+    category: ["react"],
+  },
+  {
+    name: "Todo-App",
+    image: Todo,
+    deployed_url: "https://sharp-noyce-e84569.netlify.app/",
+    github_url: "https://github.com/AbhishekGujjar17/Todo-App",
+    category: ["react.js", "frontend"],
+  },
 
-    {
-        name: 'Todo-App',
-        image: Todo,
-        deployed_url: 'https://sharp-noyce-e84569.netlify.app/',
-        github_url: 'https://github.com/AbhishekGujjar17/Todo-App',
-        category: ['react.js','frontend']
-    },
+  {
+    name: "AI-Desktop Voice Assistant",
+    image: Jarvis,
+    deployed_url:
+      "https://github.com/AbhishekGujjar17/JARVIS-AI-Desktop-Voice-Assistant-With-Python",
+    github_url:
+      "https://github.com/AbhishekGujjar17/JARVIS-AI-Desktop-Voice-Assistant-With-Python",
+    category: ["python"],
+  },
 
-    {
+  {
+    name: "TextUtils",
+    image: Textutils,
+    deployed_url: "https://textutilsabhi.netlify.app/",
+    github_url: "https://github.com/AbhishekGujjar17/Textutils",
+    category: ["react.js"],
+  },
 
-        
-        name: 'AI-Desktop Voice Assistant',
-        image: Jarvis,
-        deployed_url: 'https://github.com/AbhishekGujjar17/JARVIS-AI-Desktop-Voice-Assistant-With-Python',
-        github_url: 'https://github.com/AbhishekGujjar17/JARVIS-AI-Desktop-Voice-Assistant-With-Python',
-        category: ['python']
-    },
+  {
+    name: "NewsMonkey",
+    image: NewsMonkey,
+    deployed_url: "https://github.com/AbhishekGujjar17/NewsMonkey",
+    github_url: "https://github.com/AbhishekGujjar17/NewsMonkey",
+    category: ["react.js"],
+  },
 
-    {
-        name: 'TextUtils',
-        image: Textutils,
-        deployed_url: 'https://textutilsabhi.netlify.app/',
-        github_url: 'https://github.com/AbhishekGujjar17/Textutils',
-        category: ['react.js']
-    },
+  {
+    name: "KingDomino Board Game",
+    image: KingDomino,
+    deployed_url:
+      "https://github.com/AbhishekGujjar17/comp2005-winter22-group3",
+    github_url: "https://github.com/AbhishekGujjar17/comp2005-winter22-group3",
+    category: ["Java"],
+  },
 
-    {
-        name: 'NewsMonkey',
-        image: NewsMonkey,
-        deployed_url: 'https://github.com/AbhishekGujjar17/NewsMonkey',
-        github_url: 'https://github.com/AbhishekGujjar17/NewsMonkey',
-        category: ['react.js']
-    },
+  {
+    name: "Musicxy - MP3 Player",
+    image: Musicxy,
+    deployed_url: "https://github.com/AbhishekGujjar17/Musicxy---MP3-Player",
+    github_url: "https://github.com/AbhishekGujjar17/Musicxy---MP3-Player",
+    category: ["python"],
+  },
 
-    {
-        name: 'KingDomino Board Game',
-        image: KingDomino,
-        deployed_url: 'https://github.com/AbhishekGujjar17/comp2005-winter22-group3',
-        github_url: 'https://github.com/AbhishekGujjar17/comp2005-winter22-group3',
-        category: ['Java']
-    },
+  {
+    name: "Covid-19 Notification System",
+    image: Covidnotify,
+    deployed_url:
+      "https://github.com/AbhishekGujjar17/Covid-19-Notification-System",
+    github_url:
+      "https://github.com/AbhishekGujjar17/Covid-19-Notification-System",
+    category: ["python"],
+  },
 
-    {
-        name: 'Musicxy - MP3 Player',
-        image: Musicxy,
-        deployed_url: 'https://github.com/AbhishekGujjar17/Musicxy---MP3-Player',
-        github_url: 'https://github.com/AbhishekGujjar17/Musicxy---MP3-Player',
-        category: ['python']
-    },
+  {
+    name: "Realtime Chat Application",
+    image: Chat,
+    deployed_url: "https://realtimechatappl.netlify.app",
+    github_url: "https://github.com/AbhishekGujjar17/Realtime-Chat-Application",
+    category: ["node.js", "socket.io"],
+  },
 
-    
+  {
+    name: "Spotify Playlist Generator",
+    image: Spotify,
+    deployed_url:
+      "https://github.com/AbhishekGujjar17/Spotify_Playist_Generator",
+    github_url: "https://github.com/AbhishekGujjar17/Spotify_Playist_Generator",
+    category: ["python"],
+  },
 
-   
+  {
+    name: "Transparent Login Page",
+    image: Loginpage,
+    deployed_url: "https://loving-easley-2bd16f.netlify.app/",
+    github_url: "https://github.com/AbhishekGujjar17/Transparent-Login-Page",
+    category: ["frontend"],
+  },
 
-   
-    
-    
-    {
-        
-        name: 'Covid-19 Notification System',
-        image: Covidnotify,
-        deployed_url: 'https://github.com/AbhishekGujjar17/Covid-19-Notification-System',
-        github_url: 'https://github.com/AbhishekGujjar17/Covid-19-Notification-System',
-        category: ['python']
-    },
+  {
+    name: "Gym Website",
+    image: Gym,
+    deployed_url: "https://codepen.io/abhishekgujjar17/full/bGBvpXY",
+    github_url: "https://github.com/AbhishekGujjar17/Gym-Website",
+    category: ["frontend"],
+  },
 
-    {
-        name: 'Realtime Chat Application',
-        image: Chat,
-        deployed_url: 'https://realtimechatappl.netlify.app',
-        github_url: 'https://github.com/AbhishekGujjar17/Realtime-Chat-Application',
-        category: ['node.js','socket.io']
-    },
+  {
+    name: "Payment Form",
+    image: Form,
+    deployed_url: "https://codepen.io/abhishekgujjar17/full/qBqoNXZ",
+    github_url: "https://github.com/AbhishekGujjar17/PaymentForm",
+    category: ["frontend"],
+  },
 
+  {
+    name: "Covid-19 Map Visualization",
+    image: Covidmap,
+    deployed_url:
+      "https://github.com/AbhishekGujjar17/Plotting-Covid-19-Cases-on-World-Map",
+    github_url:
+      "https://github.com/AbhishekGujjar17/Plotting-Covid-19-Cases-on-World-Map",
+    category: ["frontend"],
+  },
 
+  {
+    name: "Send-Encrypted-Mails",
+    image: Encryption,
+    deployed_url:
+      "https://github.com/AbhishekGujjar17/Send-Encrypted-Mails-via-Python",
+    github_url:
+      "https://github.com/AbhishekGujjar17/Send-Encrypted-Mails-via-Python",
+    category: ["python"],
+  },
 
-    {
-        name: 'Spotify Playlist Generator',
-        image: Spotify,
-        deployed_url: 'https://github.com/AbhishekGujjar17/Spotify_Playist_Generator',
-        github_url: 'https://github.com/AbhishekGujjar17/Spotify_Playist_Generator',
-        category: ['python']
-    },
+  {
+    name: "Drink Water Reminder",
+    image: Notifier,
+    deployed_url:
+      "https://github.com/AbhishekGujjar17/Drink-Water-Notification-System",
+    github_url:
+      "https://github.com/AbhishekGujjar17/Drink-Water-Notification-System",
+    category: ["python"],
+  },
 
-    {
-        name: 'Transparent Login Page',
-        image: Loginpage,
-        deployed_url: 'https://loving-easley-2bd16f.netlify.app/',
-        github_url: 'https://github.com/AbhishekGujjar17/Transparent-Login-Page',
-        category: ['frontend']
-    },
+  // {
+  //     name: 'Gambling Dice Game',
+  //     image: Dice,
+  //     deployed_url: 'https://github.com/AbhishekGujjar17/Dice-Game-Python',
+  //     github_url: 'https://github.com/AbhishekGujjar17/Dice-Game-Python',
+  //     category: ['python']
+  // },
 
-    {
-        name: 'Gym Website',
-        image: Gym,
-        deployed_url: 'https://codepen.io/abhishekgujjar17/full/bGBvpXY',
-        github_url: 'https://github.com/AbhishekGujjar17/Gym-Website',
-        category: ['frontend']
-    },
-
-    {
-        name: 'Payment Form',
-        image: Form,
-        deployed_url: 'https://codepen.io/abhishekgujjar17/full/qBqoNXZ',
-        github_url: 'https://github.com/AbhishekGujjar17/PaymentForm',
-        category: ['frontend']
-    },
-
-    {
-        name: 'Covid-19 Map Visualization',
-        image: Covidmap,
-        deployed_url: 'https://github.com/AbhishekGujjar17/Plotting-Covid-19-Cases-on-World-Map',
-        github_url: 'https://github.com/AbhishekGujjar17/Plotting-Covid-19-Cases-on-World-Map',
-        category: ['frontend']
-    },
-
-    
-    
-    {
-        name: 'Send-Encrypted-Mails',
-        image: Encryption,
-        deployed_url: 'https://github.com/AbhishekGujjar17/Send-Encrypted-Mails-via-Python',
-        github_url: 'https://github.com/AbhishekGujjar17/Send-Encrypted-Mails-via-Python',
-        category: ['python']
-    },
-
-    {
-        name: 'Drink Water Reminder',
-        image: Notifier,
-        deployed_url: 'https://github.com/AbhishekGujjar17/Drink-Water-Notification-System',
-        github_url: 'https://github.com/AbhishekGujjar17/Drink-Water-Notification-System',
-        category: ['python']
-    },
-
-    {
-        name: 'Gambling Dice Game',
-        image: Dice,
-        deployed_url: 'https://github.com/AbhishekGujjar17/Dice-Game-Python',
-        github_url: 'https://github.com/AbhishekGujjar17/Dice-Game-Python',
-        category: ['python']
-    },
-
-    {
-        name: 'Image Serach App',
-        image: Imagesearch,
-        deployed_url: 'https://hopeful-clarke-a2b76b.netlify.app/',
-        github_url: 'https://github.com/AbhishekGujjar17/ReactApp',
-        category: ['react.js']
-    },
-
-    
-
-
-
-
-
-   
-]
+  // {
+  //     name: 'Image Serach App',
+  //     image: Imagesearch,
+  //     deployed_url: 'https://hopeful-clarke-a2b76b.netlify.app/',
+  //     github_url: 'https://github.com/AbhishekGujjar17/ReactApp',
+  //     category: ['react.js']
+  // },
+];
 
 export default data_projects;
--- src/components/data/resume_data.js
+++ src/components/data/resume_data.js
@@ -1,77 +1,61 @@
-
-
-
 const languages = [
-    {
-        
-        name: 'Python',
-        level: '92'
-    },
-    {
-    
-        name: 'C++',
-        level: '91'
-    },
-    {
-    
-        name: 'JavaScript',
-        level: '88'
-    },
-    {
-    
-        name: 'C',
-        level: '90'
-    },
-    {
-    
-        name: 'Java',
-        level: '88'
-    },
-    {
-    
-        name: 'SQL',
-        level: '92'
-    },
-    {
-    
-        name: 'MYSQL',
-        level: '92'
-    }
-]
+  {
+    name: "Python",
+    level: "92",
+  },
+  {
+    name: "C++",
+    level: "91",
+  },
+  {
+    name: "JavaScript",
+    level: "88",
+  },
+  {
+    name: "C",
+    level: "90",
+  },
+  {
+    name: "Java",
+    level: "88",
+  },
+  {
+    name: "SQL",
+    level: "92",
+  },
+  {
+    name: "SQLite",
+    level: "92",
+  },
+];
 
 const tools = [
-    {
-        
-        name: 'React and Redux',
-        level: '92'
-    },
-    {
-        
-        name: 'NodeJs',
-        level: '89'
-    },
-    {
-        name: 'AWS',
-        level: '90'
-    },
-    {
-        
-        name: 'MongoDB',
-        level: '92'
-    },
+  {
+    name: "React and Redux",
+    level: "92",
+  },
+  {
+    name: "NodeJs",
+    level: "89",
+  },
+  {
+    name: "AWS",
+    level: "90",
+  },
+  {
+    name: "MongoDB",
+    level: "92",
+  },
 
-    {
-        
-        name: 'Android Studio',
-        level: '88'
-    },
+  {
+    name: "Android Studio",
+    level: "88",
+  },
 
-    {
-        
-        name: 'Machine Learning',
-        level: '86'
-    },
+  {
+    name: "Machine Learning",
+    level: "86",
+  },
+];
 
-]
-
-export { tools, languages }
+export { tools, languages };
