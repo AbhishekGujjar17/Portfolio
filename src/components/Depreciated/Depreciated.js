8f586e98aed68cdedb78971d2452bf1c7f4ac7c8
Abhishek Gujjar <70919722+AbhishekGujjar17@users.noreply.github.com>
2023-05-07 02:43:53 -0230

    Create Depreciated.js

---
+++ src/components/Depreciated/Depreciated.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Depreciated = () => {
+  return (
+    <div className="alert alert-danger d-flex align-items-center d-flex justify-content-center" role="alert">
+      This site is no longer updated. Please visit:  <a href="https://abhishekgujjar.netlify.app/" target="__blank" className="alert-link">
+        &nbsp;abhishekgujjar.netlify.app
+      </a>
+    </div>
+  );
+};
+
+export default Depreciated;
